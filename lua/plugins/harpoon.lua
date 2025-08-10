85fe3ac8b4ffd9ea8bb0e370285054b035e083c8
pandeyanup <pandeyanup58@gmail.com>
2025-08-10 18:53:59 +0545

    fix: add missing add file to harpoon
    - [x] add telescope buffers and jumplists

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -15,9 +15,17 @@ return {
         {
           "<leader>h",
           function()
+            local harpoon = require "harpoon"
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = "Harpoon quick menu",
+        },
+        {
+          "<leader>ha",
+          function()
             require("harpoon"):list():add()
           end,
-          desc = "Harpoon file",
+          desc = "Add file to harpoon",
         },
         {
           "<leader>hh",
