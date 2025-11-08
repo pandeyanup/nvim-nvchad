4468366705977ac93ad7d735358dca6d153c48bb
hernem <kawaii.neko.zero@gmail.com>
2025-11-08 10:47:14 +0545

    chore: rounded window on lsp hover

--- lua/options.lua
+++ lua/options.lua
@@ -5,6 +5,7 @@ local opt = vim.opt
 
 opt.number = true
 opt.relativenumber = true
+vim.opt.winborder = "rounded"
 
 -- o.cursorlineopt ='both' -- to enable cursorline!
 o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
